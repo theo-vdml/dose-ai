cfbb1491133dd65094b3926d297464552821f926
Théo Vandormael <theo.vandormael@proton.me>
2026-01-27 01:36:49 +0100

    fix: add display=swap to custom font to improve TTFF

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,7 +39,7 @@
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" rel="stylesheet" />
 
     @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
     @inertiaHead
